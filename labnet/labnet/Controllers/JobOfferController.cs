9b7ba755addcc25cea46c70d185c7ef073828a3d
Zhanna Solobchuk <zhozhyk.sol@gmail.com>
2019-10-26 10:48:45 +0200

    Displaying all job offers

--- labnet/labnet/Controllers/JobOfferController.cs
+++ labnet/labnet/Controllers/JobOfferController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using labnet.Models;
 
 namespace labnet.Controllers
 {
     public class JobOfferController : Controller
     {
+
+        private static List<JobOffer> _jobOffers = new List<JobOffer>
+        {
+            new JobOffer{Id=1, JobTitle= "Backend Developer" },
+            new JobOffer{Id=1, JobTitle= "Frontend Developer" },
+            new JobOffer{Id=1, JobTitle= "Manager" },
+            new JobOffer{Id=1, JobTitle= "Cook" }
+        };
+
+
         [Route("joboffer/index")]
         public IActionResult Index()
         {
-            return View();
+            
+            return View(_jobOffers);
         }
     }
 }
